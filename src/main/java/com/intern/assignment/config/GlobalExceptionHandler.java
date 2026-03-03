bc8376f9d7c492684759be48b7abe5de668dacd5
bgovil <bgovil@ciena.com>
2026-03-03 15:58:46 +0530

    added separate exception handler function to handle bad request exceptions

--- src/main/java/com/intern/assignment/config/GlobalExceptionHandler.java
+++ src/main/java/com/intern/assignment/config/GlobalExceptionHandler.java
@@ -12,13 +12,23 @@ import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
-    @ExceptionHandler({DeviceNotFoundException.class, ShelfNotFoundException.class, ShelfPositionNotFoundException.class, DeviceCannotBeCreatedException.class, ShelfPositionCannotBeCreatedException.class, ShelfCannotBeCreatedException.class})
-    public ResponseEntity<Map<String,Object>> handleExceptions(Exception exception) {
+    @ExceptionHandler({DeviceNotFoundException.class, ShelfNotFoundException.class, ShelfPositionNotFoundException.class})
+    public ResponseEntity<Map<String,Object>> handleNotFoundExceptions(Exception exception) {
         Map<String,Object> response = new HashMap<>();
         response.put("message", exception.getMessage());
         response.put("status", HttpStatus.NOT_FOUND.value());
         response.put("timestamp", LocalDateTime.now());
         response.put("error", "found nothing");
         return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler({DeviceCannotBeCreatedException.class, ShelfPositionCannotBeCreatedException.class, ShelfCannotBeCreatedException.class})
+    public ResponseEntity<Map<String, Object>> handleBadRequestExceptions(Exception exception) {
+        Map<String,Object> response = new HashMap<>();
+        response.put("message", exception.getMessage());
+        response.put("status", HttpStatus.BAD_REQUEST.value());
+        response.put("timestamp", LocalDateTime.now());
+        response.put("error", "incorrect input");
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
     }
 }
